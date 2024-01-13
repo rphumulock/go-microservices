60453c284f7f7410352ac1bd871fff0ac0842c5e
Richard <rphumulock@gmail.com>
2024-01-13 00:47:27 -0500

    Added docker/dockercompose

--- broker-service/cmd/api/main.go
+++ broker-service/cmd/api/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const webPort = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting Broker Service on port %s\n", webPort)
+
+	// Define Http Server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// Start the server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 }
--- broker-service/go.mod
+++ broker-service/go.mod
@@ -1,3 +1,8 @@
 module broker
 
-go 1.21.5
+go 1.18
+
+require (
+	github.com/go-chi/chi/v5 v5.0.11
+	github.com/go-chi/cors v1.2.1
+)
